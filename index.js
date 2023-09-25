cad99136e16631ba302b3c468069682341366de1
nkwaaaa <nkwaaaa@gmail.com>
2023-09-25 09:06:57 -0300

    add list users

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
-  const newUser = await prisma.user.create({
-    data: {
-      name: "Mati",
-      email: "mati@gmail.com",
-    },
-  })
-  console.log(newUser)
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     name: "Mati",
+  //     email: "mati@gmail.com",
+  //   },
+  // })
+  // console.log(newUser)
+
+  const users = await prisma.user.findMany()
+
+  console.log(users)
 }
 
 main()
