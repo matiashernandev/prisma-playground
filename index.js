6d6b0ce35c872d3014b790f4ffcebbe0d3b2368f
nkwaaaa <nkwaaaa@gmail.com>
2023-09-25 09:31:27 -0300

    add delete user

--- index.js
+++ index.js
@@ -10,23 +10,33 @@ async function main() {
   //   },
   // })
   // console.log(newUser)
-
   //const users = await prisma.user.findMany()
-
-  const user = await prisma.user.findFirst({
-    where: {
-      //name: "Mati",
-
-      OR: [
-        {
-          id: 55,
-        },
-        { email: "mati@gmail.com" },
-      ],
-    },
-  })
-
-  console.log(user)
+  /* -------------------------------------------------------------------------- */
+  // const user = await prisma.user.findFirst({
+  //   where: {
+  //     //name: "Mati",
+  //     OR: [
+  //       {
+  //         id: 55,
+  //       },
+  //       { email: "mati@gmail.com" },
+  //     ],
+  //   },
+  // })
+  /* -------------------------------------------------------------------------- */
+  //   try {
+  //     const user = await prisma.user.delete({
+  //       where: {
+  //         id: 2,
+  //       },
+  //     })
+  //     console.log(user)
+  //   } catch (error) {
+  //     if (error.code === "P2025") {
+  //       console.log("usuario no encontrado")
+  //     }
+  //     console.error(error.message)
+  //   }
 }
 
 main()
